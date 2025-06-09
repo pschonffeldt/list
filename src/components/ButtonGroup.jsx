0c7d826cd0e8e8e3b78b2fe4e236515c9a2af732
Pablo S <cineschonffeldt@gmail.com>
2025-06-09 16:24:39 -0500

    Add lib folder and constants.js

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,13 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initials",
+  "Remove all items",
+];
+
 export default function ButtonGroup() {
-  const secondaryButtons = [
-    "Mark all as complete",
-    "Mark all as incomplete",
-    "Reset to initials",
-    "Remove all items",
-  ];
   return (
     <section className="button-group">
       {secondaryButtons.map((text) => {
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,34 @@
+const InitialItems = [
+  {
+    name: "good mood",
+    packed: true,
+  },
+  {
+    name: "passport",
+    packed: false,
+  },
+  {
+    name: "phone charger",
+    packed: false,
+  },
+];
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {InitialItems.map((item) => {
+        return <Item key={item.name} item={item} />;
+      })}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" /> {item.name}
+      </label>
+    </li>
+  );
 }
