42a229111bac4677720d4cde4980dbad339f37dc
Pablo S <cineschonffeldt@gmail.com>
2025-06-10 15:18:02 -0500

    Add sort and local storage function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -7,7 +7,8 @@ import Sidebar from "./Sidebar";
 import { initialItems } from "./lib/constants";
 
 function App() {
-  const [items, setItems] = useState(initialItems);
+  const itemsFromLocalStorage = JSON.parse(localStorage.getItem("items"));
+  const [items, setItems] = useState(itemsFromLocalStorage || initialItems);
 
   const handleAddItem = (newItemText) => {
     const newItem = {
@@ -59,11 +60,18 @@ function App() {
     setItems(newItems);
   };
 
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
   return (
     <>
       <BackgroundHeading />
       <main>
-        <Header />
+        <Header
+          numberOfItemsPacked={items.filter((item) => item.packed).length}
+          totalNumberOfItems={items.length}
+        />
         <ItemList
           items={items}
           handleDeleteItem={handleDeleteItem}
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,17 +1,60 @@
+import Select from "react-select";
+import EmptyView from "./lib/EmptyView";
+import { useState } from "react";
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
+
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return;
+  });
+
   return (
-    <ul>
-      {items.map((item) => {
+    <ul className="item-list">
+      {items.length === 0 ? <EmptyView /> : null}
+
+      {items.length > 0 ? (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
+        </section>
+      ) : null}
+
+      {sortedItems.map((item) => {
         return (
           <Item
             key={item.id}
             item={item}
-            handleDeleteItem={handleDeleteItem}
-            handleToggleItem={handleToggleItem}
+            onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
           />
         );
       })}
@@ -19,19 +62,19 @@ export default function ItemList({
   );
 }
 
-function Item({ item, handleDeleteItem, handleToggleItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li className="item">
       <label>
         <input
-          onChange={() => handleToggleItem(item.id)}
+          onChange={() => onToggleItem(item.id)}
           checked={item.packed}
           type="checkbox"
         />{" "}
         {item.name}
       </label>
 
-      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
